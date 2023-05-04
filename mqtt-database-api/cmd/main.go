576f7f5ce0d60092ef640bd366d32547e1a08b15
adamhoof <adam.hofman1@gmail.com>
2023-05-04 18:04:01 +0200

    implement mqtt database api to listen to requests, query data from database about product requested and then send it to the requester

---
+++ mqtt-database-api/cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/adamhoof/MedunkaOPBarcode2.0/config"
+	"github.com/adamhoof/MedunkaOPBarcode2.0/database"
+	"github.com/adamhoof/MedunkaOPBarcode2.0/mqtt-database-api/pkg/api"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"time"
+)
+
+func main() {
+	conf, err := config.LoadConfig("/home/adamhoof/MedunkaOPBarcode2.0/Config.json")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Println("config ok")
+
+	postgresqlHandler := database.PostgreSQLHandler{}
+	err = postgresqlHandler.Connect(&conf.Database)
+	if err != nil {
+		log.Fatal("connection to database unsuccessful: ", err)
+	}
+
+	log.Println("database connected")
+
+	options := mqtt.ClientOptions{
+		ClientID:             "mqtt-database-api",
+		CleanSession:         false,
+		Order:                false,
+		AutoReconnect:        true,
+		ConnectRetryInterval: 2 * time.Second,
+		ConnectRetry:         true,
+	}
+	options.AddBroker(conf.MQTT.ServerAndPort)
+
+	mqttClient := mqtt.NewClient(&options)
+
+	for {
+		token := mqttClient.Connect()
+		if token.WaitTimeout(5*time.Second) && token.Error() == nil {
+			break
+		}
+		log.Println("mqtt client failed to connect, retrying...", token.Error())
+		time.Sleep(5 * time.Second)
+
+	}
+	log.Println("mqtt client connected")
+
+	token := mqttClient.Subscribe(conf.MQTT.ProductDataRequest, 0, api.GetProductData(&postgresqlHandler, conf))
+	if token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+}
