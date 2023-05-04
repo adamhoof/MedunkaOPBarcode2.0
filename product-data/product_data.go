35113ca2452033ba8710cb5b78a5f5df0685c7ec
adamhoof <adam.hofman1@gmail.com>
2023-05-04 18:07:07 +0200

    define ProductData and ProductDataRequest struct

---
+++ product-data/product_data.go
@@ -0,0 +1,15 @@
+package product_data
+
+type ProductData struct {
+	Name              string
+	Price             string
+	Stock             string
+	UnitOfMeasure     string
+	UnitOfMeasureKoef string
+}
+
+type ProductDataRequest struct {
+	ClientTopic       string `json:"clientTopic"`
+	Barcode           string `json:"barcode"`
+	IncludeDiacritics bool   `json:"includeDiacritics"`
+}
