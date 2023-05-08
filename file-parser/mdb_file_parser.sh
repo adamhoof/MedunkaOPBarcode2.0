f0f5f8923ce794057bb489fadbce84fb50a11659
adamhoof <adam.hofman1@gmail.com>
2023-05-08 09:05:27 +0200

    modify mdb to csv logic to comply with column requirements

--- file-parser/mdb_file_parser.sh
+++ file-parser/mdb_file_parser.sh
@@ -16,13 +16,14 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-mdb-export --row-delimiter="\n" --delimiter=";" "$1" SKz | awk -F";" 'BEGIN { OFS=";" } { print $25, $24, $54, $32, $34, $36 }' > "$2"
+mdb-export --delimiter=";" --quote='"' --escape-invisible "$1" SKz | awk -F";" 'BEGIN { OFS=";" } { print $25, $24, "\"" $54 "\"", $32, "\"" $34 "\"", "\"" $36 "\"" }' > "$2"
 
+#detect header changes
 requiredHeader="Nazev;EAN;ProdejDPH;MJ2;MJ2Koef;StavZ"
-
-normalisedFileHeader=$(sed -n '1p' "$2" | tr '[:upper:]' '[:lower:]')
-normalisedRequiredHeader=$(echo "$requiredHeader" | tr '[:upper:]' '[:lower:]')
+normalisedFileHeader=$(head -n 1 "$2" | tr '[:upper:]' '[:lower:]' | tr -d '"' | tr -d " ")
+normalisedRequiredHeader=$(echo "$requiredHeader" | tr '[:upper:]' '[:lower:]' | tr -d " ")
 
 if [ "$normalisedFileHeader" != "$normalisedRequiredHeader" ]; then
   echo "CSV file header and required header are not the same" >&2
+  exit 1
 fi
