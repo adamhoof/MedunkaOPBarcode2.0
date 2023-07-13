b712427e5a441f4f3b4388cc131ad950688644cb
adamhoof <adam.hofman1@gmail.com>
2023-07-13 15:24:40 +0200

    change json field to camel case

--- op-barcode-esp32/src/RequestSerializer.cpp
+++ op-barcode-esp32/src/RequestSerializer.cpp
@@ -3,9 +3,9 @@
 SerializationStatus serializeProductDataRequest(const ProductDataRequest& requestToSerialize, SerializedProductDataRequestBuffer& serializedRequest)
 {
     StaticJsonDocument<200> jsonDoc;
-    jsonDoc["ClientTopic"] = requestToSerialize.responseTopic;
-    jsonDoc["Barcode"] = requestToSerialize.barcode;
-    jsonDoc["IncludeDiacritics"] = requestToSerialize.includeDiacritics;
+    jsonDoc["clientTopic"] = requestToSerialize.responseTopic;
+    jsonDoc["barcode"] = requestToSerialize.barcode;
+    jsonDoc["includeDiacritics"] = requestToSerialize.includeDiacritics;
 
     if (serializeJson(jsonDoc, serializedRequest.data(), serializedRequest.size()) == 0) {
         return SERIALIZATION_FAILED;
