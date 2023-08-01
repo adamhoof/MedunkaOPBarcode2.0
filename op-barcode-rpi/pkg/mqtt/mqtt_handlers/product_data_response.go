32a4ba719987e7dd92060b7a90052bd7f27016f3
adamhoof <adam.hofman1@gmail.com>
2023-08-01 22:41:25 +0200

    add space

--- op-barcode-rpi/pkg/mqtt/mqtt_handlers/product_data_response.go
+++ op-barcode-rpi/pkg/mqtt/mqtt_handlers/product_data_response.go
@@ -50,7 +50,7 @@ func ProductDataResponseHandler() mqtt.MessageHandler {
 		}
 
 		pricePerUnitOfMeasure := floatPrice * floatPricePerUnitOfMeasureCoef
-		cli_artist.PrintStyledText(cli_artist.ItalicWhite(), fmt.Sprintf("Přepočet na %s\n: %.2f Kč", productData.UnitOfMeasure, pricePerUnitOfMeasure))
+		cli_artist.PrintStyledText(cli_artist.ItalicWhite(), fmt.Sprintf("Přepočet na %s:\n %.2f Kč", productData.UnitOfMeasure, pricePerUnitOfMeasure))
 		cli_artist.PrintSpaces(3)
 		cli_artist.PrintStyledText(cli_artist.ItalicWhite(), fmt.Sprintf("Stock: %s", productData.Stock))
 		cli_artist.PrintSpaces(2)
